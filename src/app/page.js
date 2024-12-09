d8641005e9107407a3d417bb5042d8139ccf2ac6
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-12-09 14:24:59 +0530

    welcome spelling

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@
     return (
       <div className="h-screen bg-smoke flex gap-4 justify-center items-center sm:flex-col overflow-hidden">
        
-         <p className="text-5xl mx-auto text-electric-blue">Welocome to Snatch</p>  
+         <p className="text-5xl mx-auto text-electric-blue">Welcome to Snatch</p>  
 
           <p>
           <Link className=" text-2xl" href="/signup">Click here: Signup page</Link>
