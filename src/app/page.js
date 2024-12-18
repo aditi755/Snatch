e9fea82c1e63432eff1bd0c6313fab348cac9b9b
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-12-18 20:14:37 +0530

    adding loading state in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -16,13 +16,42 @@
   // }
 
 
+// "use client";
+
+// import { useUser } from "@clerk/nextjs";
+// import Link from "next/link";
+
+// export default function Home() {
+//   const { isSignedIn } = useUser(); // Check if user is signed in
+
+//   return (
+//     <div className="h-screen bg-smoke flex gap-4 justify-center items-center sm:flex-col overflow-hidden">
+//       <p className="text-5xl mx-auto text-electric-blue">Welcome to Snatch</p>
+//       <p>
+//         <Link
+//           className="text-2xl"
+//           href={isSignedIn ? "/dashboard" : "/signup"} // Redirect based on login status
+//         >
+//           Click here: {isSignedIn ? "Dashboard" : "Signup"} page
+//         </Link>
+//       </p>
+//     </div>
+//   );
+// }
+
+
 "use client";
 
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
-  const { isSignedIn } = useUser(); // Check if user is signed in
+  const { isLoaded, isSignedIn } = useUser(); // Destructure isLoaded and isSignedIn
+
+  // Show a loading state while the user data is being hydrated
+  if (!isLoaded) {
+    return <div className=" flex justify-center items-center text-3xl h-screen">Loading...</div>;
+  }
 
   return (
     <div className="h-screen bg-smoke flex gap-4 justify-center items-center sm:flex-col overflow-hidden">
