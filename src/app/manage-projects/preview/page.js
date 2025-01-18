d84b574111107c0230f4ee18d1135eaa833b7a9b
Aditi Bhadoriya <awdhesh1700@gmail.com>
2025-01-18 21:20:31 +0530

    suspense added

--- src/app/manage-projects/preview/page.js
+++ src/app/manage-projects/preview/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import Image from "next/image";
 import { useSelectedProjects } from "../context";
 import ProjectsGrid from "@/components/ProjectsGrid";
